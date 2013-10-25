37476118017e65a50530cd97ef132b761282bba3
Christopher Deutsch <cd@cdeutsch.com>
2013-10-25 13:56:18 -0700

    Updated to correct namespace

--- App_Start/RegisterDatatablesModelBinder.cs
+++ App_Start/RegisterDatatablesModelBinder.cs
@@ -2,9 +2,10 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.WebPages;
 
-[assembly: PreApplicationStartMethod(typeof(Mvc.JQuery.Datatables.Example.App_Start.RegisterDatatablesModelBinder), "Start")]
+[assembly: PreApplicationStartMethod(typeof(Mvc.Bootstrap.Datatables.Example.App_Start.RegisterDatatablesModelBinder), "Start")]
 
-namespace Mvc.JQuery.Datatables.Example.App_Start {
+namespace Mvc.Bootstrap.Datatables.Example.App_Start
+{
     public static class RegisterDatatablesModelBinder {
         public static void Start() {
             ModelBinders.Binders.Add(typeof(DataTablesParam), new NullableDataTablesModelBinder());
